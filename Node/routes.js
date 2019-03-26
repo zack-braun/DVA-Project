76c3f19ce626d55242147d3702e9856bbfe9d3ba
Zack Braun <zack.braun@gatech.edu>
2019-03-25 22:31:29 -0400

    boilderplate for routes

--- Node/routes.js
+++ Node/routes.js
@@ -1,4 +1,12 @@
+const apiKey4ProPublica = 'gKufsGOW6vGeCqfYQiVNkuITq7h1fytjlfaL1Qtq';
+const apiKey4OpenSecrets = 'd3f8992dc4e2d3ad0d1c50fcbecea064';
+
 // reveals main.js properties to routes
 module.exports = function (app) {
+  app.get('/', (req, res) => res.send('hello world'));
 
+  app.get('/allUser', (req, res) => {
+    // pass
+  });
 };
+
