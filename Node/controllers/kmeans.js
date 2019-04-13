c250599997f3d01ea153a2c32f184f2de7c7f905
zack-braun <zack.braun@gatech.edu>
2019-04-13 18:47:55 -0400

    fixed small bug

--- Node/controllers/kmeans.js
+++ Node/controllers/kmeans.js
@@ -3,15 +3,19 @@ const kmeans = {
 
   initData: async (congressmenClass) => {
     const arr = await congressmenClass.getAll();
-    let finalArr;
+    const finalArr = [];
     for (let i = 0; i < arr.length; i += 1) {
       const financeDist = congressmenClass.parseFinanceData(arr[i]);
       const dw = arr[i].dw_nominate;
-      finalArr = [dw, finnaceDist.fir, financeDist.dgr, financeDist.afc, financeDist.le, financeDist.et];
+      finalArr[i] = [dw, financeDist.health, financeDist.fir, financeDist.dgr, financeDist.afc, financeDist.le, financeDist.et];
     }
+    // console.log(finalArr);
     return finalArr;
   },
 
+  startKmeans: async (congressmenClass) => {
+    arr = await kmeans.initData(congressmenClass);
+  },
 
 };
 
